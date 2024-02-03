6ba42ba0f6e39231543ace925eba1a89c2e900b5
ashkan nazari <ashkannazari248@gmail.com>
2024-02-03 21:56:36 +0330

    fix(app): edit app.vue

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage/>
+  </NuxtLayout>
+</template>
+<script setup>
+useHead({
+  htmlAttrs: {
+    lang: 'fa',
+    dir: 'rtl'
+  }
+})
+</script>
+<style lang="sass">
+@use "@/assets/sass/main"
+</style>
