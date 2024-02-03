571dad051c472828506d8a146649a8c5518e142e
ashkan nazari <ashkannazari248@gmail.com>
2024-02-03 22:31:33 +0330

    fix(): fix baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   ssr: false,
+  app: {
+    baseURL: '/sarmayex/'
+  },
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', "@nuxt/image"],
   plugins: [],
